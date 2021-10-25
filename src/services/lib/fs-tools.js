d9ebeb0803ff15ef756e0788653b1abb3e8c7a7c
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-10-25 16:40:10 +0200

    posting image

--- src/services/lib/fs-tools.js
+++ src/services/lib/fs-tools.js
@@ -3,7 +3,7 @@ import fs from "fs-extra";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 
-const { readJson, writeJson } = fs;
+const { readJson, writeJson, writeFile } = fs;
 
 const dataFolder = join(dirname(fileURLToPath(import.meta.url)), "../data");
 
@@ -14,3 +14,6 @@ const publicFolderPath = join(process.cwd(), "./public/img/products");
 export const getProducts = () => readJson(producsJSONpath);
 
 export const writeProducts = (content) => writeJson(producsJSONpath, content);
+
+export const saveProductsImages = (fileName, contentAsBuffer) =>
+  writeFile(join(publicFolderPath, fileName), contentAsBuffer);
