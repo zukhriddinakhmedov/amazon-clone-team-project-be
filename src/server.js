18b79c5f47c4392ae32161d4ceace0142f9f3b0a
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-10-25 11:05:25 +0200

    templete create

---
+++ src/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
+import { join } from "path";
+
+import productsRouter from "./services/products/index.js";
+
+const server = express();
+
+// ******************** Global middlewares **********************
+
+server.use(cors());
+server.use(express.json());
+
+// ************************ ENDPOINTS **********************
+const staticFolderPath = join(process.cwd(), "./public");
+server.use(express.static(staticFolderPath));
+
+server.use("/products", productsRouter);
+
+// ************************ END **********************
+
+// ******************** ERROR MIDDLEWARES **********************
+// here all error handlers are generic to all the endpoints
+
+// ******************** END ERROR MIDDLEWARES **********************
+
+const port = 3001;
+
+console.table(listEndpoints(server));
+
+server.listen(port, () => {
+  console.log("Server running on port:", port);
+});
